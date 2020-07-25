93c5ef84e8685ebb314e2f983695628939393e25
paul <ajuzepaul>
2020-07-25 23:52:47 +0300

    add invite button on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @pending_request = current_user.pending_friends
+    @received_requests = current_user.friend_requests
   end
 end
