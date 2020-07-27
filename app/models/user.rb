95238f67319d431afe7b2924fab4e487e0a1d397
paul <ajuzepaul>
2020-07-28 02:06:42 +0300

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # rubocop:disable Lint/ShadowingOuterLocalVariable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -15,47 +14,22 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friend?(user)
-    self.friends.include?(user)
+    friends.include?(user)
   end
 
   def mutual_friends?(user)
-    self.friend?(user) and user.friend?(self)
+    friend?(user) and user.friend?(self)
   end
 
   def confirm_friend(user)
-    self.friends << user
+    friends << user
   end
 
   def pending_friends
-    self.friendships.map { |friendship| friendship.friend unless friendship.friend.friend?(self) }.compact
+    friendships.map { |friendship| friendship.friend unless friendship.friend.friend?(self) }.compact
   end
 
   def friend_requests
-    self.inverse_friendships.map { |friendship| friendship.user unless self.friend?(friendship.user) }.compact
+    inverse_friendships.map { |friendship| friendship.user unless friend?(friendship.user) }.compact
   end
-
-  # def friends
-  #   friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-  #   friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-  #   friends_array.compact
-  # nd
-
-  # def pending_friends
-  #   friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  # end
-
-  # def friend_requests
-  #   inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
-  # end
-
-  # def confirm_friend(user)
-  #   friendship = inverse_friendships.find { |friendship| friendship.user == user }
-  #   friendship.confirmed = true
-  #   friendship.save
-  # end
-
-  # def friend?(user)
-  #   friends.include?(user)
-  # end
 end
-# rubocop:enable Lint/ShadowingOuterLocalVariable
