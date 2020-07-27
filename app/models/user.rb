b9aa72d6f2d8b60872334fea65112346b66fa696
paul <ajuzepaul>
2020-07-28 01:22:59 +0300

    modify user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,31 +10,48 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-    friends_array.compact
-  end
-
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
-
-  def friend_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  def friend?(user)
+    self.friends.include?(user)
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
-    friendship.confirmed = true
-    friendship.save
+    self.friends << user
   end
 
-  def friend?(user)
-    friends.include?(user)
+  def pending_friends
+    self.friendships.map { |friendship| friendship.friend unless friendship.friend.friend?(self) }.compact
   end
+
+  def friend_requests
+    self.inverse_friendships.map { |friendship| friendship.user unless self.friend?(friendship.user) }.compact
+  end
+
+  # def friends
+  #   friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+  #   friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
+  #   friends_array.compact
+  # nd
+
+  # def pending_friends
+  #   friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  # end
+
+  # def friend_requests
+  #   inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  # end
+
+  # def confirm_friend(user)
+  #   friendship = inverse_friendships.find { |friendship| friendship.user == user }
+  #   friendship.confirmed = true
+  #   friendship.save
+  # end
+
+  # def friend?(user)
+  #   friends.include?(user)
+  # end
 end
 # rubocop:enable Lint/ShadowingOuterLocalVariable
